70452f05bfea64e7a78107a2bf10ec1eedd32320
Szumi6760 <szymon.przeklasa@onet.pl>
2023-05-08 18:38:19 +0200

    Zapisywanie statków na planszy gracza naprawione :D

--- gra.py
+++ gra.py
@@ -3,6 +3,7 @@ import os
 from colorama import Fore, Back, Style
 import time
 import sys
+import random
 
 # Menu Sterujące
 def Menu():
@@ -19,20 +20,23 @@ def Stworz_plansze(Rozmiar):
         Plansza.append(["O"] * Rozmiar)
     return Plansza
 
+Plansza_gracza = Stworz_plansze(7)
+Plansza_komputera = Stworz_plansze(7)
+
 def Wyswietl_plansze_gracza(Plansza): #Wyświetlanie planszy gracza
     for Rzad in Plansza:
-        print("                              " + " ".join(Rzad))
+        print("                              " + " ".join(Rzad), end="")
 
 def Wyswietl_plansze_komputera(Plansza): #Wyświetlanie planszy komputera
     for Rzad in Plansza:
-        print("                      " + " ".join(Rzad))
+        print("                      " + " ".join(Rzad), end="")
 
-def Wyswietl_obie_plansze(Plansza):
-    for Rzad in Plansza:
-        print("                              " + " ".join(Rzad) + "                      " + " ".join(Rzad), sep="")
+def Wyswietl_obie_plansze(Plansza_gracza, Plansza_komputera):
+    for i in range(0, len(Plansza)):
+        print("                              " + " ".join(Plansza_gracza[i]) + "                      " + " ".join(Plansza_komputera[i]), sep="")
 
 def Aktualizuj_plansze_gracza(Plansza, Kolumna_gracza, Wiersz_gracza):
-    Plansza[Wiersz_gracza][Kolumna_gracza] = Style.BRIGHT + Fore.BLACK + "S" + Fore.WHITE
+    Plansza_gracza[Wiersz_gracza][Kolumna_gracza] = Style.BRIGHT + Fore.BLACK + "S" + Fore.WHITE
     os.system('cls')
     print(Style.BRIGHT + Fore.WHITE + "                         [" + Style.BRIGHT + Fore.BLUE + "+" +Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
     print(Style.BRIGHT + Fore.GREEN + "                                                   Twój ruch")
@@ -41,23 +45,35 @@ def Aktualizuj_plansze_gracza(Plansza, Kolumna_gracza, Wiersz_gracza):
     print(Style.BRIGHT + Fore.WHITE + "                              Twoja Plansza:                   Plansza Komputera:         " + Fore.GREEN + "X" + Fore.WHITE + " - trafione pole")
     print(Style.BRIGHT + Fore.WHITE + "                                                                                          " + Fore.RED + "X" + Fore.WHITE + " - nietrafione pole")
     
-    Wyswietl_obie_plansze(Plansza)
+    Wyswietl_obie_plansze(Plansza_gracza, Plansza_komputera)
+
+def Aktualizuj_plansze_komputera(Plansza, Kolumna_komputera, Wiersz_komputera):
+    Plansza_komputera[Wiersz_komputera][Kolumna_komputera] = Style.BRIGHT + Fore.BLACK + "S" + Fore.WHITE
+    os.system('cls')
+    print(Style.BRIGHT + Fore.WHITE + "                         [" + Style.BRIGHT + Fore.BLUE + "+" +Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
+    print(Style.BRIGHT + Fore.RED + "                                                   Ruch komputera")
+    print(Style.BRIGHT + Fore.WHITE + "                                                                                          O - puste pole")
+    print(Style.BRIGHT + Fore.BLACK + "                                                                                          S" + Fore.WHITE + " - pole ze statkiem")
+    print(Style.BRIGHT + Fore.WHITE + "                              Twoja Plansza:                   Plansza Komputera:         " + Fore.GREEN + "X" + Fore.WHITE + " - trafione pole")
+    print(Style.BRIGHT + Fore.WHITE + "                                                                                          " + Fore.RED + "X" + Fore.WHITE + " - nietrafione pole")
+    
+    Wyswietl_obie_plansze(Plansza_gracza, Plansza_komputera)
 
 # Losowanie pozycji statków komputera
 def Losuj_pozycje(Plansza):
-    Pozycja = random.randint(0, len(plansza) - 1)
+    Pozycja = random.randint(0, len(Plansza) - 1)
     return Pozycja
 
 # Rozstawianie statków na planszy (gracza)
 def Rozstawianie_statkow_gracza(Plansza, Ilosc_statkow):
     for i in range(Ilosc_statkow):
-        print(Style.BRIGHT + Fore.WHITE +"\n                         [" + Style.BRIGHT + Fore.BLUE + "+" +Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
+        print(Style.BRIGHT + Fore.WHITE +"\n                         [" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
         print("Pozycja Statku #%s" % (i+1))
         while True:
             Kolumna_gracza = int(input("Podaj kolumnę statku: ")) - 1
             time.sleep(0.2)
             Wiersz_gracza = int(input("Podaj wiersz statku: ")) - 1
-            if Plansza[Wiersz_gracza][Kolumna_gracza] != "S":
+            if Plansza_gracza[Wiersz_gracza][Kolumna_gracza] != "S":
                 Aktualizuj_plansze_gracza(Plansza, Kolumna_gracza, Wiersz_gracza)
                 break
             else:
@@ -69,11 +85,12 @@ def Rozstawianie_statkow_komputera(Plansza, Ilosc_statkow):
         while True:
             Kolumna_komputera = Losuj_pozycje(Plansza)
             Wiersz_komputera = Losuj_pozycje(Plansza)
-            if Plansza[Kolumna_komputera][Wiersz_komputera] != "S":
-                Plansza[Kolumna_komputera][Wiersz_komputera] = "S"
+            if Plansza_komputera[Kolumna_komputera][Wiersz_komputera] != "S":
+                Aktualizuj_plansze_komputera(Plansza, Kolumna_komputera, Wiersz_komputera)
                 break
 
 while True:
+    
     os.system('cls') # Wyczyszczenie konsoli
     Menu() # Wywołanie Menu Głównego
     opcja = int(input("Opcja: "))
@@ -96,29 +113,27 @@ while True:
         Plansza = Stworz_plansze(Rozmiar_planszy)
         
         Ruch = "gracz"
+        
         print(Style.BRIGHT + Fore.WHITE + "                         [" + Style.BRIGHT + Fore.BLUE + "+" +Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
         if Ruch == "gracz":
             print(Style.BRIGHT + Fore.GREEN + "                                                   Twój ruch")
-            print(Style.BRIGHT + Fore.WHITE + "                                                                                         O - puste pole")
-            print(Style.BRIGHT + Fore.BLACK + "                                                                                         S" + Fore.WHITE + " - pole ze statkiem")
-            print(Style.BRIGHT + Fore.WHITE + "                              Twoja Plansza:                   Plansza Komputera:        " + Fore.GREEN + "X" + Fore.WHITE + " - trafione pole")
-            print(Style.BRIGHT + Fore.WHITE + "                                                                                         " + Fore.RED + "X" + Fore.WHITE + " - nietrafione pole")
-            Wyswietl_obie_plansze(Plansza)
-            #Wyswietl_plansze_gracza(Plansza)
-            #Wyswietl_plansze_komputera(Plansza)
-            Rozstawianie_statkow_gracza(Plansza, Ilosc_statkow)
-
-        elif Ruch == "komputer":
-            print(Style.BRIGHT + Fore.RED + "                                               Ruch komputera")
             print(Style.BRIGHT + Fore.WHITE + "                                                                                          O - puste pole")
             print(Style.BRIGHT + Fore.BLACK + "                                                                                          S" + Fore.WHITE + " - pole ze statkiem")
             print(Style.BRIGHT + Fore.WHITE + "                              Twoja Plansza:                   Plansza Komputera:         " + Fore.GREEN + "X" + Fore.WHITE + " - trafione pole")
             print(Style.BRIGHT + Fore.WHITE + "                                                                                          " + Fore.RED + "X" + Fore.WHITE + " - nietrafione pole")
-            Wyswietl_plansze_komputera(Plansza)
+            Wyswietl_obie_plansze(Plansza_gracza, Plansza_komputera)
+            Rozstawianie_statkow_gracza(Plansza, Ilosc_statkow)
+        elif Ruch == "komputer":
+            print(Style.BRIGHT + Fore.RED + "                                               Ruch komputera")
+            print(Style.BRIGHT + Fore.WHITE + "                                                                                           O - puste pole")
+            print(Style.BRIGHT + Fore.BLACK + "                                                                                           S" + Fore.WHITE + " - pole ze statkiem")
+            print(Style.BRIGHT + Fore.WHITE + "                              Twoja Plansza:                   Plansza Komputera:          " + Fore.GREEN + "X" + Fore.WHITE + " - trafione pole")
+            print(Style.BRIGHT + Fore.WHITE + "                                                                                           " + Fore.RED + "X" + Fore.WHITE + " - nietrafione pole")
+            Wyswietl_obie_plansze(Plansza_gracza, Plansza_komputera)
             Rozstawianie_statkow_komputera(Plansza, Ilosc_statkow)
         print(Style.BRIGHT + Fore.WHITE +"\n                         [" + Style.BRIGHT + Fore.BLUE + "+" +Style.BRIGHT + Fore.WHITE + "]" + Style.BRIGHT + Fore.BLUE + "-------------------------------------------------------" + Style.BRIGHT + Fore.WHITE + "[" + Style.BRIGHT + Fore.BLUE + "+" + Style.BRIGHT + Fore.WHITE + "]")
         
-        input()
+
 
     # Autorzy
     elif opcja == 2:
